2179ffa05981a4379f85cff521d8d5b3feb890b5
Yaël Busser <yael.busser@gmail.com>
2024-04-03 15:47:25 +0200

    :sparkles: feature add ball physic

--- src/components/CarController/index.jsx
+++ src/components/CarController/index.jsx
@@ -25,46 +25,53 @@ const CarController = ({state, controls}) => {
     const car = useRef();
     const {rotationSpeed, carSpeed} = useControls({
         rotationSpeed: {
-            value: 3,
+            value: 500,
             min: 0,
             max: 10,
             step: 0.1
         },
         carSpeed: {
-            value: 3,
+            value: 1.5,
             min: 0,
             max: 100,
-            step: 0.01
+            step: 0.005
         }
     });
 
     const lookAt = useRef(new Vector3(0, 0, 0));
+    const isJumping = useRef(false);
+    let isOnGround;
     useFrame(({camera}, delta) => {
         if (!car.current) {
             return;
         }
-
+        const currentPosition = car.current.translation();
+        isOnGround = Math.abs(currentPosition.y) < 0.5;
+        if (isOnGround) {
+            isJumping.current = false;
+        }
         if (me.id === state?.id) {
             const targetLookAt = vec3(car.current.translation());
             lookAt.current.lerp(targetLookAt, 0.1);
             camera.lookAt(lookAt.current);
         }
         const currentRotationVelocity = car.current.angvel();
-        if (controls.isJoystickPressed()) {
+        if (controls.isJoystickPressed() && isOnGround) {
             const angle = controls.angle();
-            const direction = angle > Math.PI / 2 ? 1 : -1;
+            const direction = angle > Math.PI / 2 ? 0.3 : -0.3;
             currentRotationVelocity.y = -direction * Math.sin(angle) * rotationSpeed;
 
             const impulse = vec3({
                 x: 0,
                 y: 0,
-                z: (CAR_SPEED[carModel] || carSpeed) * 2 * direction
+                z: (CAR_SPEED[carModel] || carSpeed) * direction
             });
 
             const eulerRotation = euler().setFromQuaternion(quat(car.current.rotation()));
             impulse.applyEuler(eulerRotation);
             car.current.applyImpulse(impulse, true);
         }
+
         car.current.setAngvel(currentRotationVelocity, true);
         // S'assurer de calculer une seule fois, donc passer par l'host
         if (isHost()) {
@@ -81,7 +88,20 @@ const CarController = ({state, controls}) => {
             respawn();
         }
     }, []);
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.code === "Space" && isOnGround && me?.id === state?.id) {
+                const impulse = vec3({x: 0, y: 10, z: 0});
+                car.current.applyImpulse(impulse, true);
+            }
+        };
 
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
     const respawn = () => {
         if (isHost()) {
             car.current.setTranslation({
@@ -90,7 +110,7 @@ const CarController = ({state, controls}) => {
                 z: randInt(-2, 2) * 4,
             });
             car.current.setLinvel({x: 0, y: 0, z: 0});
-            car.current.setRotation({x: 0, y: 0, z: 0, w: 1});
+            car.current.setRotation({x: 0, y: 0, z: 0, w: 0.5});
             car.current.setAngvel({x: 0, y: 0, z: 0});
         }
     };
@@ -111,7 +131,7 @@ const CarController = ({state, controls}) => {
                     }
                 }}
             >
-                <Html>
+                <Html position-y={1}>
                     <div className={"game-profile"}>
                         <img className={"profile-picture"} src={state.state.profile.photo}/>
                         <h1 className={"nickname"} style={{color: state.state.profile.color || "white"}}>
@@ -119,10 +139,10 @@ const CarController = ({state, controls}) => {
                         </h1>
                     </div>
                 </Html>
-                <Cars model={carModel} scale={1.3}/>
+                <Cars model={carModel} scale={0.5}/>
                 {
                     me.id === state.id && (
-                        <PerspectiveCamera makeDefault position={[-2, 3, -5]} fov={70}/>
+                        <PerspectiveCamera makeDefault position={[-2, 1, -5]} near={1} fov={50}/>
                     )
                 }
             </RigidBody>
--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,8 +1,9 @@
-import {Environment, Gltf, Lightformer, OrbitControls} from "@react-three/drei";
-import {CuboidCollider, Physics, RigidBody} from "@react-three/rapier";
-import {useEffect, useState} from "react";
+import {Environment, Gltf, Html, Lightformer, OrbitControls, Sphere} from "@react-three/drei";
+import {CuboidCollider, Physics, RigidBody, BallCollider} from "@react-three/rapier";
+import {useEffect, useRef, useState} from "react";
 import {Joystick, onPlayerJoin} from "playroomkit";
 import CarController from "../CarController/index.jsx";
+import {useFrame} from "@react-three/fiber";
 
 const Game = () => {
     const [players, setPlayers] = useState([]);
@@ -19,6 +20,21 @@ const Game = () => {
             });
         }, []);
     }, []);
+    const ballRef = useRef();
+    const [ballPosition, setBallPosition] = useState([0, 0, 0]);
+    useFrame(() => {
+        // Vérifier si la référence à la balle existe
+        console.log(ballRef.current.position.x)
+        if (ballRef.current) {
+            const position = ballRef.current.position;
+            // Mettre à jour l'état avec la nouvelle position
+            setBallPosition(position);
+        }
+        //console.log(ballRef.current);
+    });
+    useEffect(() => {
+        console.log(ballRef);
+    }, []);
     const scaleMap = 5;
     return (
         <group>
@@ -32,7 +48,7 @@ const Game = () => {
                     target={[0, 0, 0]} // Target position (optional = undefined)
                 />
             </Environment>
-            <pointLight position={[0, 5, 0]} intensity={2.5} distance={10} />
+            <pointLight position={[0, 5, 0]} intensity={2.5} distance={10}/>
             <pointLight
                 position={[5, 5, 0]}
                 intensity={10.5}
@@ -45,7 +61,7 @@ const Game = () => {
                 distance={15}
                 color="blue"
             />
-            <directionalLight position={[10, 10, 10]} intensity={0.4} />
+            <directionalLight position={[10, 10, 10]} intensity={0.4}/>
             <Physics>
                 {
                     players.map(({state, controls}, index) => (
@@ -62,10 +78,21 @@ const Game = () => {
                     position-y={-5}
                     name={"void"}
                 >
-                    <CuboidCollider args={[100, 3, 100]} />
+                    <CuboidCollider args={[100, 3, 100]}/>
+                </RigidBody>
+                <RigidBody
+                    type={"dynamic"}
+                    colliders={"ball"}
+                    gravityScale={1}
+                    position={[0,0.5,0]}
+                >
+                    <Sphere ref={ballRef} args={[0.5,50,50]}>
+                        <meshNormalMaterial />
+                    </Sphere>
                 </RigidBody>
             </Physics>
             <ambientLight intensity={0.5}/>
+            <Html position={[0, 2, 0]}>{`Position de la balle : ${ballPosition.x}`}</Html>
         </group>
     )
 }
